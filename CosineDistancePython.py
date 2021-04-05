b6e4eb8a39461404afddead24f04735872317580
Prasanth Medicharla <79551118+prasanthmsvs@users.noreply.github.com>
2021-04-05 16:06:58 +0530

    Create CosineDistancePython.py

---
+++ CosineDistancePython.py
@@ -0,0 +1,9 @@
+from scipy import spatial 
+a = [1, 2]
+b = [2, 4]
+c = [2.5, 4] 
+d = [4.5, 5] 
+spatial.distance.cosine(c,a)
+spatial.distance.cosine(c,b)
+spatial.distance.cosine(c,d)
+spatial.distance.cosine(a,b)
